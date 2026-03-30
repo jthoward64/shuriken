0dadf8c68bef52f574c39042259b1d5a19534d4d
Joshua Tag Howard <git@jthoward.dev>
2026-03-30 14:57:38 -0400

    feat: add initial database schema and relations for auth, calendar, card, and group management
    - Created new schema files for auth, calendar, card, casbin, dav, groups, principal, and user.
    - Defined relations between entities in the new relations.ts file.
    - Implemented various tables with appropriate fields, types, and constraints.
    - Added custom types for handling specific data formats such as timestamp with timezone and JSONB.
    - Established indexes and unique constraints to optimize database queries and ensure data integrity.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "drizzle-kit";
 
 // biome-ignore lint/style/noDefaultExport: required by drizzle-kit
 export default defineConfig({
-	out: "./src/drizzle",
-	schema: "./src/drizzle/schema/index.ts",
+	out: "./src/db/drizzle",
+	schema: "./src/db/drizzle/schema/index.ts",
 	dialect: "postgresql",
 	dbCredentials: {
 		// biome-ignore lint/style/noNonNullAssertion: drizzle will handle missing value
